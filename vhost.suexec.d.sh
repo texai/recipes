103a1bc732b014f6137f2f5e2a7edb195365fcba
Ernesto Anaya <ernestex@gmail.com>
2012-10-10 03:43:55 -0500

    alternativa

--- vhost.suexec.d.sh
+++ vhost.suexec.d.sh
@@ -3,4 +3,12 @@ echo "Content-type: text/html"
 echo ""
 echo "<pre>"
 whoami
+
+#OPTION 1
 /usr/bin/git --git-dir=/home/texai/www/recipes/.git --work-tree=/home/texai/www/recipes pull --progress >> log
+
+
+#OPTION 2
+cd /home/texai/www/recipes
+/usr/bin/git --git-dir=/home/texai/www/recipes/.git fetch
+/usr/bin/git --git-dir=/home/texai/www/recipes/.git --work-tree=/home/texai/www/recipes merge origin/master
